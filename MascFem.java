8ec85df879cfeb87ced13f893a960999eae33ac5
lazariniuser <gabriellazarinicontato@gmail.com>
2023-08-24 21:15:09 -0300

    bidimensional

--- MascFem.java
+++ MascFem.java
@@ -1,37 +1,27 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class MascFem {
     public static void main(String[] args) {
         Boolean play = true;
         Scanner scanner = new Scanner(System.in);
-        int contadorElas = 0;
-        int contadorEles = 0;
-        String[] listaElas = new String[10];
-        String[] listaEles = new String[10];
+        int contador = 0;
+        String[][] cadastrados = new String[10][2];
 
         System.out.println(
-                "Olá! Boas vindas ao centro de inscrição para a candidatura às vagas para alunos da escola Calltech.");
+                "Olá! Boas vindas ao centro de inscrição para a candidatura às vagas para alunos da escola Caltech.");
 
         while (play) {
 
             System.out.println("Por favor, insira o nome do candidato.");
             String nome = scanner.nextLine();
-
             System.out.println("Por favor, insira o gênero do candidato. 'f' para feminino e 'm' para masculino.");
             String gen = scanner.nextLine();
 
-            if (gen.equalsIgnoreCase("f")) {
+            cadastrados[contador][0] = nome;
+            cadastrados[contador][1] = gen.equalsIgnoreCase("f") ? "Feminino" : "Masculino";
 
-                listaElas[contadorElas] = ("Nome: " + nome + "; Gênero: Feminino");
-                contadorElas++;
-
-            }
-
-            if (gen.equalsIgnoreCase("m")) {
-                listaEles[contadorEles] = ("Nome: " + nome + "; Gênero: Masculino");
-                contadorEles++;
-
-            }
+            contador++;
 
             System.out.println(
                     "Deseja inscrever mais alguém? Digite 's' para sim, e 'n' para não.");
@@ -41,20 +31,12 @@ public class MascFem {
                 play = false;
             }
 
-            System.out.println("Segue as listas de alunos e alunas inscritas:");
+        }
 
-            for (int i = 0; i < contadorEles; i++) {
-                System.out.println((i + 1) + "º Aluno: " + listaEles[i] + ". ");
-            }
-
-            for (int i = 0; i < contadorElas; i++) {
-                System.out.println((i + 1) + "ª Aluna: " + listaElas[i] + ". ");
-            }
-
-            System.out.println(
-                    "Temos um total de " + contadorElas + " alunas e " + contadorEles + " alunos, contabilizando "
-                            + (contadorElas + contadorEles) + " candidatos inscritos para estudar na escola.");
+        for (int i = 0; i < cadastrados[contador].length; i++) {
+            System.out.println((i + 1) + ": " + cadastrados[i][0] + "; Gênero: " + cadastrados[i][1]);
 
         }
+        System.out.println("Total de " + (contador - 1) + " candidatos inscritos para estudar na escola.");
     }
 }
